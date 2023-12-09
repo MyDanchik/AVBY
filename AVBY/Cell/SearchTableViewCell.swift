5fabfaec8fcf7c7fd27050db86e9dd0eda21cfc7
Daniil <idanik.mega@gmail.com>
2023-12-09 20:39:42 +0300

    config UI

--- AVBY/Cell/SearchTableViewCell.swift
+++ AVBY/Cell/SearchTableViewCell.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class SearchTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var conteinerView: UIView!
+        
     @IBOutlet weak var nameCarLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
+        conteinerView.layer.cornerRadius = 10
     }
 
     
